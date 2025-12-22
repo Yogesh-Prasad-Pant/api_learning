8bed45952d7f30c9b21e2968e874b60167cb83f5
Yogesh-Prasad-Pant <pant444yogesh@gmail.com>
2025-12-22 20:31:13 +0545

    Setup Admin model, migration and auth guards

---
+++ app/Models/Admin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable, CanResetPassword;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'role',
+        'contact_no',
+        'address',
+        'password',
+        'status',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+}
