cb48e1be6cd5cc6fe4bbdf1dea479e28ca02f4c7
Yogesh-Prasad-Pant <pant444yogesh@gmail.com>
2025-12-23 00:43:35 +0545

    Add updateProfile logic and fixed validations

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminAuthController extends Controller
 {
+
+    // function for login 
     public function login(Request $request)
     {
         $credentials = $request->validate([
@@ -30,12 +32,34 @@ class AdminAuthController extends Controller
         ], 401);
     }
 
+
+
+// function for logout
     public function logout(Request $request)
     {
        if($request->user()){
         $request->user()->currentAccessToken()->delete();
        }
         return response()->json(['message' => 'Logged out successfully']);
+    }
+
+
+
+
+// function for updating the admins detail
+    public function updateProfile(Request $request){
+
+        $admin = $request->user(); 
+        $data = $request->validate([
+            'name' => ['sometimes', 'string', 'max:255'],
+            'contact_no' => ['sometimes', 'string', 'max:15'],
+            'address' => ['sometimes', 'string'],
+        ]);
+        $admin->update($data);
+        $admin->refresh();
+        return response()->json([
+            'message'=> 'Profile updated Successfully',
+            'admin' => $admin],200);
     }
     //
 }
--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminAuthController;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+        return $request->user();
+    })->middleware('auth:sanctum');
 
 
 // public route for login
 Route::post('/admin/login', [AdminAuthController::class, 'login']);
-// protected routes fro logout
+
+// protected routes for updating profile and  logout
 
 Route::middleware('auth:sanctum')->group(function (){
-Route::post('/admin/logout',[AdminAuthController::class, 'logout'])
-;});
+        Route::post('/admin/logout',[AdminAuthController::class, 'logout']);
+        Route::put('/admin/update',[AdminAuthController::class, 'updateProfile']);
+    });
