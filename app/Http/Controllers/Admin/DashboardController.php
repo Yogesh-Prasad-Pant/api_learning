b51b22103ba04463d81499125efc947bd85b372e
Yogesh-Prasad-Pant <pant444yogesh@gmail.com>
2026-02-16 22:54:22 +0545

    dashboard controller created and is active middleware is created and return index function  with details

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    //function to return detail to logined user or dasboard
     public function index(Request $request)
     {
         $admin = $request->user();
@@ -28,5 +29,21 @@ class DashboardController extends Controller
             ]
         ]);
     }
-    //
+
+    //function to show superadmin control pannel
+    public function superIndex()
+    {
+        return response()->json([
+            'stats' => [
+                'total_admin' => Admin::count(),
+                'pending_kyc' => Admin::where('kyc_status','pending')->count(),
+                'active_now' => Admin::where('status', 'active')->count(),
+                'suspended' => Admin::where('status', 'suspended')->count(),
+            ],
+            'quick_links'=>[
+                'search_admins' => url('/api/admin/list/search'),
+            ]
+        ]);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,30 +10,36 @@ use App\Http\Controllers\Admin\DashboardController;
 Route::prefix('admin')->group(function (){
 
     // public route for register admin and login
-        Route::middleware('throttle:5,1')->group(function (){
+        Route::middleware('throttle:5,1')->group(function ()
+        {
             Route::post('/register-request',[AdminAuthController::class, 'registerRequest']);
             Route::post('/login', [AdminAuthController::class, 'login']);
         });
     // password forget/reset route
-        Route::middleware('throttle:3,1')->group(function(){
+        Route::middleware('throttle:3,1')->group(function()
+        {
             Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLink']);
             Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
         });
     // protected routes for viewing  searching and updating profile and  logout
-        Route::middleware('auth:admin')->group(function (){
+        Route::middleware('auth:admin')->group(function ()
+        {
             Route::post('/logout',[AdminAuthController::class, 'logout']);
             Route::post('/change-password', [AdminAuthController::class, 'changePassword']);
             Route::put('/update',[AdminAuthController::class, 'updateProfile']);
             Route::post('/update-image',[AdminAuthController::class, 'updateImage']);
             Route::delete('/delete',[AdminManagementController::class, 'deleteAdmin']);
             Route::post('/upload-kyc', [AdminAuthController::class, 'uploadKyc']);
-            });
+        });
     //routes for only active admins
-        Route::middleware(['auth:admin', 'is_active'])->group(function (){
+        Route::middleware(['auth:admin', 'is_active'])->group(function ()
+        {
             Route::get('/dashboard', [DashboardController::class, 'index']);
         });
     // Routes that only super_admin can access
-        Route::middleware(['auth:admin','super_admin'])->group(function(){
+        Route::middleware(['auth:admin','super_admin'])->group(function()
+        {
+            Route::get('/super/dashboard', [DashboardController::class, 'superIndex']);
             Route::get('/list',[AdminManagementController::class, 'index'])->name('admin.index');
             Route::get('/kyc/view/{id}/{type}', [AdminManagementController::class, 'viewDocument'])->name('admin.kyc.view');
             Route::post('/kyc/change-status/{id}', [AdminManagementController::class, 'changeKycStatus'])->name('admin.kyc.status');
