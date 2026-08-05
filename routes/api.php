c7fc4f1725dac8647db17efa1a537345477093e8
Yogesh-Prasad-Pant <pant444yogesh@gmail.com>
2026-08-05 23:56:27 +0545

    cors phile configured in config setting for borwser only local host for next.js and vue.js placed

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\CategoryController;
-
-/*
-|--------------------------------------------------------------------------
-| Shared & Public API Routes (/api/*)
-|--------------------------------------------------------------------------
-*/
-
-// Generates: /api/categories
-Route::get('/categories', [CategoryController::class, 'index']);
